7081e41995b5340438f76cffc8e504aa44685bac
Marius <marius.contoli-legay@epitech.eu>
2020-01-02 18:10:13 +0100

    handled cone and erros on others

--- 104intersection.py
+++ 104intersection.py
@@ -25,22 +25,22 @@ def result(delta, a, _b) :
         print("({0:.3f}, {1:.3f}, {2:.3f})".format(xx, yy, zz))
     elif delta > 0 :
         print("2 intersection points:")
-        t = (-_b + math.sqrt(delta) / (2 * a))
+        t = (-_b + math.sqrt(delta)) / (2 * a)
         xx = xp + xv * t
         yy = yp + yv * t
         zz = zp + zv * t
         print("({0:.3f}, {1:.3f}, {2:.3f})".format(xx, yy, zz))
-        t = (-_b - math.sqrt(delta) / (2 * a))
+        t = (-_b - math.sqrt(delta)) / (2 * a)
         xx = xp + xv * t
         yy = yp + yv * t
         zz = zp + zv * t
         print("({0:.3f}, {1:.3f}, {2:.3f})".format(xx, yy, zz))
 
 def sphere() :
-    print("Sphere of radius", p)
+    print("Sphere of radius", int(p))
     print("Line passing through the point (",int(xp),", ",int(yp),", ",int(zp),") and parallel to the vector (",int(xv),", ",int(yv),", ",int(zv),")", sep="")
     a = math.pow(xv, 2) + math.pow(yv, 2) + math.pow(zv, 2)
-    x_2 = 2 * xp * zv
+    x_2 = 2 * xp * xv
     y_2 = 2 * yp * yv
     z_2 = 2 * zp * zv
     _b = x_2 + y_2 + z_2
@@ -49,33 +49,39 @@ def sphere() :
     if a == 0 :
         sys.exit(84)
     
-    delta = math.pow(_b, 2) - 4 * a * _c
+    delta = (_b * _b) - (4 * a * _c)
     result(delta, a, _b)
         
 
 def cylinder() :
-    print("Cylinder of radius", p)
+    print("Cylinder of radius", int(p))
     print("Line passing through the point (",int(xp),", ",int(yp),", ",int(zp),") and parallel to the vector (",int(xv),", ",int(yv),", ",int(zv),")", sep="")
     a = math.pow(xv, 2) + math.pow(yv, 2)
-    x_2 = 2 * xp * zv
+    x_2 = 2 * xp * xv
     y_2 = 2 * yp * yv
-    z_2 = 2 * zp * zv
-    _b = x_2 + y_2 + z_2
+    _b = x_2 + y_2
     _c = math.pow(xp, 2) + math.pow(yp, 2) - math.pow(p, 2)
-    delta = math.pow(_b, 2) - 4 * a * _c
-    result(delta, a, _b)
+    delta = (_b * _b) - (4 * a * _c)
+    if not a == 0 :
+        result(delta, a, _b)
     if a == 0 :
         if zv == 0 :
             sys.exit(84)
-        else : 
-            if _c == 0 : 
-                print("There is an infinite number of intersection points.")
+        else :  
+            print("There is an infinite number of intersection points.")
 
 def cone() :
     print("Cone with a",int(p) ,"degree angle")
     print("Line passing through the point (",int(xp),", ",int(yp),", ",int(zp),") and parallel to the vector (",int(xv),", ",int(yv),", ",int(zv),")", sep="")
+    radian = (math.pi / 2) - (p * math.pi) / 180.0
+    tan_calc = math.pow(math.tan(radian), 2)
+    a = tan_calc * (math.pow(xv, 2) + math.pow(yv, 2)) - math.pow(zv, 2)
+    _b = tan_calc * (2 * xp * xv + 2 * yp * yv) - 2 * zp * zv
+    _c = tan_calc * (math.pow(xp, 2) + math.pow(yp, 2)) - math.pow(zp, 2) 
+    delta = (_b * _b) - (4 * a * _c)
+    result(delta, a, _b)
 
-if not len(sys.argv) > 1:
+if not len(sys.argv) > 7:
     sys.exit(84)
 if opt == 1 :
     sphere()
